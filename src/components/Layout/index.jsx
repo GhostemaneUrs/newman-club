6053eaccc1c25dac137b4f077f3fbca5f7c075c5
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-04-25 14:48:49 -0500

    feat: update structure component layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,7 +2,7 @@ import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 export const Layout = ({ children }) => {
   return (
-    <div className='flex flex-col min-h-screen'>
+    <div className='flex flex-col min-h-screen w-full'>
       <Header />
       <main className='flex-1-auto w-full'>{children}</main>
       <Footer />
