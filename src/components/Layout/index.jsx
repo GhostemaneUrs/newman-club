cac05d6be6b2c963059335827d6379479e13f5d3
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-04-25 04:19:37 -0500

    feat: implement structure header and layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -4,9 +4,7 @@ export const Layout = ({ children }) => {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <main className='flex-1-auto w-full max-w-[1440px] m-auto px-4'>
-        {children}
-      </main>
+      <main className='flex-1-auto w-full'>{children}</main>
       <Footer />
     </div>
   )
