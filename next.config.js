0c4b903204772f8cf4028de1def1ba7f0e6d6ef1
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-04-25 04:19:55 -0500

    feat: implement config videos webpack and structure folders jsconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com']
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.(mp4|webm)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/videos/',
+            outputPath: 'static/videos/',
+            name: '[name].[ext]'
+          }
+        }
+      })
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
